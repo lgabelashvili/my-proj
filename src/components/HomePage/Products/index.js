20fa9673cf830a4f1e9a8fb24242d5715246f940
gabelashvili <gabelashvili1999@gmail.com>
2021-06-16 22:30:54 +0400

    fx

--- src/components/HomePage/Products/index.js
+++ src/components/HomePage/Products/index.js
@@ -1,9 +1,58 @@
 import {
   Div,
+  Title,
+  containerStyles,
+  FindService,
+  SelectCategory,
+  Box,
+  CategoryList,
+  BoxTitle,
+  BoxDesc,
+  LearnMore,
 } from './styles';
+import Container from '../../Container';
 
 const Products = () => (
-  <Div>qwd</Div>
+  <Div>
+    <Container costumeStyles={containerStyles}>
+      <Title>PRODUCTS</Title>
+      <FindService>Find the Service that suits you</FindService>
+      <SelectCategory>Select a category and compare the best details</SelectCategory>
+      <CategoryList>
+        <Box>
+          <BoxTitle>Auto Insurance</BoxTitle>
+          <BoxDesc>
+            While driving the road is full of surprises.
+            You can protect your finances and use 24 hours auto
+          </BoxDesc>
+          <LearnMore>Learn More</LearnMore>
+        </Box>
+        <Box>
+          <BoxTitle>Auto Loans</BoxTitle>
+          <BoxDesc>
+            {`Auto loans are secured loans that use the car you're
+              buying as collateral. You're typically asked to pay a fixed`}
+          </BoxDesc>
+          <LearnMore>Learn More</LearnMore>
+        </Box>
+        <Box>
+          <BoxTitle>Legal Help</BoxTitle>
+          <BoxDesc>
+            Carloans Legal Help is for people who are handling their legal problems without a lawyer
+          </BoxDesc>
+          <LearnMore>Learn More</LearnMore>
+        </Box>
+        <Box>
+          <BoxTitle>Auto Insurance</BoxTitle>
+          <BoxDesc>
+            While driving the road is full of surprises.
+            You can protect your finances and use 24 hours auto
+          </BoxDesc>
+          <LearnMore>Learn More</LearnMore>
+        </Box>
+      </CategoryList>
+    </Container>
+  </Div>
 );
 
 export default Products;
--- src/components/HomePage/Products/styles.js
+++ src/components/HomePage/Products/styles.js
@@ -1,8 +1,162 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import BgImg from '../../../assets/images/productBg.png';
 
 export const Div = styled.div`
   background-color: #246BA3;
-  height: 600px;
   background-image: url(${BgImg});
+  padding: 40px 0;
+`;
+
+export const containerStyles = css`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.p`
+  color: #FAC04C;
+  font-size: 16px;
+  font-family: Sofia-Bold;
+`;
+
+export const FindService = styled.h1`
+  font-family: Sofia-Black;
+  font-size: 40px;
+  color: #fff;
+  margin: 20px 0;
+  @media (${({ theme }) => theme.device.laptopL}) { 
+      font-size: 36px;
+  }
+  @media (${({ theme }) => theme.device.laptop}) { 
+      font-size: 28px;
+      margin: 18px 0;
+  }
+   @media (${({ theme }) => theme.device.tablet}) { 
+      font-size: 24px;
+      margin: 14px 0;
+  }
+   @media (${({ theme }) => theme.device.mobileL}) { 
+      font-size: 20px;
+  }
+  @media (${({ theme }) => theme.device.mobileS}) { 
+      font-size: 17px;
+  }
+`;
+
+export const SelectCategory = styled.p`
+    font-family: Sofia-Medium;
+    font-size: 20px;
+    color: #d7e5f1;
+    text-align: center;
+      @media (${({ theme }) => theme.device.laptop}) { 
+      font-size: 17px;
+  }
+    @media (${({ theme }) => theme.device.mobileL}) { 
+      font-size: 15px;
+  }
+`;
+
+export const CategoryList = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  margin-top: 90px;
+    @media (${({ theme }) => theme.device.laptopL}) { 
+    grid-template-columns: repeat(2, minmax(max-content, 0));
+    justify-content: center;
+  }
+   @media (${({ theme }) => theme.device.laptop}) { 
+    grid-template-columns: repeat(2, 1fr);
+    justify-content: center;
+    margin-top: 70px;
+    & div:nth-child(2n+1){
+        justify-self: flex-end;
+    }
+    @media (${({ theme }) => theme.device.tablet}) { 
+      grid-template-columns: repeat(1, 1fr);
+      justify-content: center;
+      margin-top: 50px;
+       & div:nth-child(n){
+        justify-self: center;
+    }
+    }
+  }
+`;
+
+export const BoxTitle = styled.h2`
+    font-family: Sofia-Black;
+    font-size: 22px;
+      @media (${({ theme }) => theme.device.laptopL}) { 
+        font-size: 20px;
+      }
+      @media (${({ theme }) => theme.device.laptop}) { 
+          font-size: 18px;
+      }
+`;
+
+export const BoxDesc = styled.p`
+    font-family: Sofia-Medium;
+    font-size: 18px;
+    text-align: center;
+    margin: 24px 0 40px;
+    @media (${({ theme }) => theme.device.laptopL}) { 
+        font-size: 17px;
+     }
+    @media (${({ theme }) => theme.device.laptop}) { 
+        font-size: 14px;
+    }
+    @media (${({ theme }) => theme.device.tablet}) { 
+       margin: 24px 0 30px;
+    }
+`;
+
+export const LearnMore = styled.p`
+    font-family: Sofia-Medium;
+    font-size: 20px;
+    text-align: center;
+    color: #fac04c;
+    cursor: pointer;
+    background-color: transparent;
+    border-radius: 10px;
+    width: 100%;
+    padding: 10px;
+    @media (${({ theme }) => theme.device.laptopL}) { 
+        font-size: 18px;
+      }
+    @media (${({ theme }) => theme.device.laptop}) { 
+          font-size: 16.5px;
+    }
+`;
+
+export const Box = styled.div`
+  background-color: #1D5581;
+  max-width: 364px;
+  max-height: 300px;
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+  box-shadow: 20px 20px 16px rgba(0, 0, 0, 0.03);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  padding: 20px;
+  transition: background 0.6s;
+  cursor: context-menu;
+  &:hover {
+    background-color: #FFFFFF;
+  }
+  &:hover ${BoxTitle} {
+    color: black;
+  }
+   &:hover ${BoxDesc} {
+    color: #556573;
+  }
+   &:hover ${LearnMore} {
+      background-color: #fac04c;
+      color: white;
+  }
 `;
