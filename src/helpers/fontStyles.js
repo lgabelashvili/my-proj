b803fde063a9c7e4bd1bb7b811f0562a15ae3767
gabelashvili <gabelashvili1999@gmail.com>
2021-06-23 17:04:42 +0400

    ref

--- src/helpers/fontStyles.js
+++ src/helpers/fontStyles.js
@@ -1,20 +1,34 @@
 import { createGlobalStyle } from 'styled-components';
-import SofiaBlack from '../assets/fonts/Sofia Pro Black Az.otf';
-import SofiaBlackItalic from '../assets/fonts/Sofia Pro Black Italic Az.otf';
-import SofiaBold from '../assets/fonts/Sofia Pro Bold Az.otf';
-import SofiaBoldItalic from '../assets/fonts/Sofia Pro Bold Italic Az.otf';
-import SofiaExtraLight from '../assets/fonts/Sofia Pro ExtraLight Az.otf';
-import SofiaExtraLightItalic from '../assets/fonts/Sofia Pro ExtraLight Italic Az.otf';
-import SofiaLight from '../assets/fonts/Sofia Pro Light Az.otf';
-import SofiaLightItalic from '../assets/fonts/Sofia Pro Light Italic Az.otf';
-import SofiaMedium from '../assets/fonts/Sofia Pro Medium Az.otf';
-import SofiaMediumItalic from '../assets/fonts/Sofia Pro Medium Italic Az.otf';
-import SofiaRegular from '../assets/fonts/Sofia Pro Regular Az.otf';
-import SofiaRegularItalic from '../assets/fonts/Sofia Pro Regular Italic Az.otf';
-import SofiaSemiBold from '../assets/fonts/Sofia Pro Semi Bold Az.otf';
-import SofiaSemiBoldItalic from '../assets/fonts/Sofia Pro Semi Bold Italic Az.otf';
-import SofiaUltraLight from '../assets/fonts/Sofia Pro UltraLight Az.otf';
-import SofiaUltraLightItalic from '../assets/fonts/Sofia Pro UltraLight Italic Az.otf';
+import SofiaBlack from '../assets/fonts/Sofia Pro Black Az.woff';
+import SofiaBlackItalic from '../assets/fonts/Sofia Pro Black Italic Az.woff';
+import SofiaBold from '../assets/fonts/Sofia Pro Bold Az.woff';
+import SofiaBoldItalic from '../assets/fonts/Sofia Pro Bold Italic Az.woff';
+import SofiaExtraLight from '../assets/fonts/Sofia Pro ExtraLight Az.woff';
+import SofiaExtraLightItalic from '../assets/fonts/Sofia Pro ExtraLight Italic Az.woff';
+import SofiaLight from '../assets/fonts/Sofia Pro Light Az.woff';
+import SofiaLightItalic from '../assets/fonts/Sofia Pro Light Italic Az.woff';
+import SofiaMedium from '../assets/fonts/Sofia Pro Medium Az.woff';
+import SofiaRegular from '../assets/fonts/Sofia Pro Regular Az.woff';
+import SofiaRegularItalic from '../assets/fonts/Sofia Pro Regular Italic Az.woff';
+import SofiaSemiBold from '../assets/fonts/Sofia Pro Semi Bold Az.woff';
+import SofiaSemiBoldItalic from '../assets/fonts/Sofia Pro Semi Bold Italic Az.woff';
+import SofiaUltraLight from '../assets/fonts/Sofia Pro UltraLight Az.woff';
+import SofiaUltraLightItalic from '../assets/fonts/Sofia Pro UltraLight Italic Az.woff';
+import SofiaBlackWoff2 from '../assets/fonts/Sofia Pro Black Az.woff2';
+import SofiaBlackItalicWoff2 from '../assets/fonts/Sofia Pro Black Italic Az.woff2';
+import SofiaBoldWoff2 from '../assets/fonts/Sofia Pro Bold Az.woff2';
+import SofiaBoldItalicWoff2 from '../assets/fonts/Sofia Pro Bold Italic Az.woff2';
+import SofiaExtraLightWoff2 from '../assets/fonts/Sofia Pro ExtraLight Az.woff2';
+import SofiaExtraLightItalicWoff2 from '../assets/fonts/Sofia Pro ExtraLight Italic Az.woff2';
+import SofiaLightWoff2 from '../assets/fonts/Sofia Pro Light Az.woff2';
+import SofiaLightItalicWoff2 from '../assets/fonts/Sofia Pro Light Italic Az.woff2';
+import SofiaMediumWoff2 from '../assets/fonts/Sofia Pro Medium Az.woff2';
+import SofiaRegularWoff2 from '../assets/fonts/Sofia Pro Regular Az.woff2';
+import SofiaRegularItalicWoff2 from '../assets/fonts/Sofia Pro Regular Italic Az.woff2';
+import SofiaSemiBoldWoff2 from '../assets/fonts/Sofia Pro Semi Bold Az.woff2';
+import SofiaSemiBoldItalicWoff2 from '../assets/fonts/Sofia Pro Semi Bold Italic Az.woff2';
+import SofiaUltraLightWoff2 from '../assets/fonts/Sofia Pro UltraLight Az.woff2';
+import SofiaUltraLightItalicWoff2 from '../assets/fonts/Sofia Pro UltraLight Italic Az.woff2';
 
 const FontStyles = createGlobalStyle`
 * {
@@ -23,86 +37,141 @@ const FontStyles = createGlobalStyle`
   padding: 0;
 }
 
+@font-face {  
+    font-family: 'Custom Font'; 
+    font-display: swap; /* Read next point */
+    src: local('Custom Font'),     
+    url('/fonts/custom-font.woff2') format('woff2'),    
+    url('/fonts/custom-font.woff') format('woff')
+}
+
 @font-face {
     font-family: Sofia-Black;
-    src: url('${SofiaBlack}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Black'),     
+    url('${SofiaBlackWoff2}') format('woff2'),    
+    url('${SofiaBlack}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Black-Italic;
-    src: url('${SofiaBlackItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Black-Italic'),     
+    url('${SofiaBlackItalicWoff2}') format('woff2'),    
+    url('${SofiaBlackItalic}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Bold;
-    src: url('${SofiaBold}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Bold'),     
+    url('${SofiaBoldWoff2}') format('woff2'),    
+    url('${SofiaBold}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Bold-Italic;
-    src: url('${SofiaBoldItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Bold-Italic'),     
+    url('${SofiaBoldItalicWoff2}') format('woff2'),    
+    url('${SofiaBoldItalic}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Extra-Light;
-    src: url('${SofiaExtraLight}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Extra-Light'),     
+    url('${SofiaExtraLightWoff2}') format('woff2'),    
+    url('${SofiaExtraLight}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Extra-Light-Italic;
-    src: url('${SofiaExtraLightItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Extra-Light-Italic'),     
+    url('${SofiaExtraLightItalicWoff2}') format('woff2'),    
+    url('${SofiaExtraLightItalic}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Light;
-    src: url('${SofiaLight}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Light'),     
+    url('${SofiaLightWoff2}') format('woff2'),    
+    url('${SofiaLight}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Light-Italic;
-    src: url('${SofiaLightItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Light-Italic'),     
+    url('${SofiaLightItalicWoff2}') format('woff2'),    
+    url('${SofiaLightItalic}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Medium;
-    src: url('${SofiaMedium}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Medium'),     
+    url('${SofiaMediumWoff2}') format('woff2'),    
+    url('${SofiaMedium}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Medium-Italic;
-    src: url('${SofiaMediumItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Medium'),     
+    url('${SofiaMediumWoff2}') format('woff2'),    
+    url('${SofiaMedium}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Medium-Regular;
-    src: url('${SofiaRegular}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Medium-Regular'),     
+    url('${SofiaRegularWoff2}') format('woff2'),    
+    url('${SofiaRegular}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Medium-Regular-Italic;
-    src: url('${SofiaRegularItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Medium-Regular-Italic'),     
+    url('${SofiaRegularItalicWoff2}') format('woff2'),    
+    url('${SofiaRegularItalic}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Semi-Bold;
-    src: url('${SofiaSemiBold}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Semi-Bold;'),     
+    url('${SofiaSemiBoldWoff2}') format('woff2'),    
+    url('${SofiaSemiBold}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Semi-Bold-Italic;
-    src: url('${SofiaSemiBoldItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Semi-Bold-Italic'),     
+    url('${SofiaSemiBoldItalicWoff2}') format('woff2'),    
+    url('${SofiaSemiBoldItalic}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Ultra-Light;
-    src: url('${SofiaUltraLight}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Ultra-Light;'),     
+    url('${SofiaUltraLightWoff2}') format('woff2'),    
+    url('${SofiaUltraLight}') format('woff')
 }
 
 @font-face {
     font-family: Sofia-Ultra-Light-Italic;
-    src: url('${SofiaUltraLightItalic}') format("opentype");
+    font-display: swap; /* Read next point */
+    src: local('Sofia-Ultra-Light-Italic'),     
+    url('${SofiaUltraLightItalicWoff2}') format('woff2'),    
+    url('${SofiaUltraLightItalic}') format('woff')
 }
-
 
 `;
 
