3b2b8008c68095fb9aa80b3e16e60c295361e7f8
gabelashvili <gabelashvili1999@gmail.com>
2021-07-06 15:33:34 +0400

    fx

---
+++ src/components/Products/ProductDetails/companyInfo.js
@@ -0,0 +1,82 @@
+export const companyInfo = {
+  autoLoans: [
+    {
+      id: '1',
+      name: 'blue-sky',
+      logo: 'blueSkyLogo.png',
+      cover: 'blueSkyCover.png',
+      url: 'https://redirect-3.net/d/37627',
+      hS: [
+        '8/10',
+        'Good, Bad or No Credit Accepted',
+        '400+ Lenders Compete for Your Business ',
+        'Same Day Car Financing ',
+        'Approvals Absolutely FREE & Confidential',
+      ],
+      overview: `Looking to get auto financing in a fast friendly environment? You've come to the right place! BlueSky's auto
+     finance platform is integrated with 400+ lenders and auto dealers that provide both direct-to-consumer car financing and bad credit options through local dealerships.
+     We specialize in helping people that may have experienced credit problems or had difficulties getting car financing in the past. And our service is fast, easy & FREE.`,
+      benefits: `BlueSky's platform is integrated with actual lenders that offer bad credit auto financing directly to consumers.
+      This gives you the opportunity to get approved instantly online before going to a dealership and allows you to shop like a cash buyer. 
+      We work with our own network of auto dealers that have a dedicated department that specializes in providing auto financing for people with bad credit. 
+      Through our partnerships with auto dealers, we are able to offer alternate financing options for people that may have been turned away by traditional banks or finance companies.
+       Our service is absolutely FREE, completely secure and only takes a couple of minutes to complete. That's it! There is no pressure or obligation to use the financing once you qualify. Remember,
+      it doesn't matter if you have good credit, bad credit, no credit or even bankruptcies. We can help you get you the car you need - in as little as 24 hours.`,
+    },
+    {
+      id: '2',
+      name: 'carloans',
+      logo: 'carLoansLogo.png',
+      cover: 'carLoansCover.png',
+      url: 'https://redirect-3.net/d/52083',
+      hS: [
+        '9/10',
+        'Get a quote free of charge',
+        'Fast-track fully online service',
+        'All credit scores accepted',
+        'Nationwide network of dealerships and lenders',
+      ],
+      overview: 'Car Loans Canada is Canada’s #1 choice for vehicle financing. We serve Canadians with good and bad credit across Canada.',
+      benefits: `Car Loans Canada provides customers with a non-embarrassing, non-confrontational approach to Canadian car loans.
+       No money down needed, No SIN required to pre-qualify, No obligation, All credit accepted`,
+    },
+    {
+      id: '3',
+      name: 'cars-fast',
+      logo: 'carsFastLogo.png',
+      cover: 'carsFastCover.png',
+      url: 'https://redirect-3.net/d/60912',
+      hS: [
+        '8/10',
+        'Get a quote free of charge',
+        'Better Rates',
+        'Low Payments',
+        'Save Money',
+      ],
+      overview: `Get Approved Today through the carsfast LendingArch network of Dealers across Canada. 
+      Our auto finance network specializes in assisting people with all types of credit including consumer proposals! Your job is your credit.`,
+      benefits: `You could qualify for a loan with a lower interest rate, which would mean lower monthly payments. We let you compare multiple lenders anywhere in Canada. 
+      This means you can get lower monthly payments. Our goal is to save you money! Apply Now to see what you quality for.`,
+    },
+  ],
+  personalFinance: [
+    {
+      id: '4',
+      name: 'mana-loans',
+      logo: 'manaLoansLogo.png',
+      cover: 'manaLoansCover.png',
+      url: 'https://redirect-3.net/d/17862',
+      hS: [
+        '9/10',
+        'Financing When You Need It',
+        'Fairer Financing Without Confusion',
+        'Nationwide Network of Lenders',
+      ],
+      overview: `You provide Mana Loans with the basic details of how much you’d like to borrow and they’ll
+       find you a lender with a loan that can work for you. Mana loans cooperate with a network of personal finance specialists working across the United States competing to give people the best rates. 
+      They have a Trustpilot rating of 9/10 and are one of the USA’s most popular personal financing services`,
+      benefits: `Mana Loans is not a direct lender. They connect people who are looking for personal loans with a network of nationwide lenders. Mana Loans have developed online loan request 
+      form to help make it easier for you to find lenders that will work with you. They do not charge any fees for using Their service.`,
+    },
+  ],
+};
